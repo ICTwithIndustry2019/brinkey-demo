87fb6e1ebe6602a44af40aeb83db4a17ea00cd28
Alex Brandsen <alex.brandsen@gmail.com>
2019-02-11 18:23:59 +0100

    Added the results check

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ $("#drop").droppable({ accept: ".dissertation",
         //console.log("drop");
         var dropped = ui.draggable;
         var droppedOn = $(this);
-        $(dropped).detach().appendTo(droppedOn);    
+        $(dropped).detach().appendTo(droppedOn);   // actually move element to new container
+
+        // set up brinkey options and save gold brinkeys
+        suggestedBrinkeys = dropped.data('suggested-brinkeys').split(';');
+        goldBrinkeys = dropped.data('gold-brinkeys').split(';');
+        $('#suggested-brinkey-container').html(''); // empty div, in case there's any left over from previous analysis
+        for(key in suggestedBrinkeys){
+            $('#suggested-brinkey-container').append('<p class="suggested-brinkey" data-brinkey="'+suggestedBrinkeys[key]+'">'+suggestedBrinkeys[key]+' <span class="ui-icon ui-icon-arrow-4"></span></p>');
+        }
+        makeBrinkeysDroppable()
+         
         $('#darkness').fadeIn();
         $('#loading').fadeIn();
         setTimeout(function(){ 
@@ -15,25 +25,49 @@ $("#drop").droppable({ accept: ".dissertation",
                 $('#loading').fadeOut();
                 $('#select-brinkeys').fadeIn();
             });
-        }, 2000);
+        }, 100);
     }
 });
 
-// making the brinkeys drop work
-$(".suggested-brinkey").draggable({ cursor: "crosshair", revert: "invalid"});
-$("#selected-brinkeys-drop").droppable({ accept: ".suggested-brinkey", 
-    drop: function(event, ui) {
-        var dropped = ui.draggable;
-        var droppedOn = $(this);
-        $(dropped).detach().appendTo(droppedOn); 
-        if($('#brinkey-placeholder')){
-            $('#brinkey-placeholder').hide();
+function makeBrinkeysDroppable(){
+    // making the brinkeys drop work
+    $(".suggested-brinkey").draggable({ cursor: "crosshair", revert: "invalid"});
+    $("#selected-brinkeys-drop").droppable({ accept: ".suggested-brinkey", 
+        drop: function(event, ui) {
+            var dropped = ui.draggable;
+            var droppedOn = $(this);
+            $(dropped).detach().appendTo(droppedOn); 
+            if($('#brinkey-placeholder')){
+                $('#brinkey-placeholder').hide();
+            }
         }
-    }
-});
+    });
+}
 
 // display results after button click
 $('#check-result').click(function(){
-    console.log('check results');
-    // show overlap between brinkeys from KB and user
+    correctBrinkeys = 0;
+    $('.suggested-brinkey').each(function(){
+        if(goldBrinkeys.includes($(this).data('brinkey'))){
+            $(this).addClass('correct').children('span').removeClass('ui-icon-arrow-4').addClass('ui-icon-check');
+            if($(this).parent().attr('id')=='selected-brinkeys-drop'){ // only count correctly identified brinkeys
+                correctBrinkeys++;
+            }
+        }
+        else if($(this).parent().attr('id')=='selected-brinkeys-drop') { // only show wrong answers in selected field
+            $(this).addClass('incorrect').children('span').removeClass('ui-icon-arrow-4').addClass('ui-icon-close');
+        }
+    });
+    correctRatio = correctBrinkeys / goldBrinkeys.length;
+    
+    if(correctRatio == 0){
+        message = "Helaas, je hebt geen correcte keywords aangegeven!";
+    }
+    else if(correctRatio < 1){
+        message = "Prima gedaan, je hebt er een paar goed!";
+    }
+    else if(correctRatio == 1){
+        message = "Alles goed, geweldig!";
+    }
+    alert(message);
 });
